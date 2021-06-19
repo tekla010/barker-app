c6087e867d95c6d80c27c0047595d38aaff3c289
teksif <60664947+teksif@users.noreply.github.com>
2021-06-19 10:50:08 -0500

    Uploading Barker App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <React.StrictMode>
+
+      <div>
+        <Navbar bg="light" expand="lg">
+            <Link activeClassName="active" exact to="/">Homepage</Link>
+            <Link activeClassName="active" to="/Auth">Auth View</Link>
+            <Link activeClassName="active" to="/Home">Home View</Link>
+          </Navbar>
+      </div>
+      <header className="App-header">
+        Welcome to the bookshelf!
+      </header>
+
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={App}
+        />
+        <Route
+          path='/Auth'
+          component={Login}
+        />
+      </Switch>
+    </React.StrictMode>
+  </HashRouter>,
   document.getElementById('root')
 );
 
